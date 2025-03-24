ce8213397ad2255c2b243a65bcead16a916d006b
rishabh21g <biasedengineer@gmail.com>
2025-03-24 10:27:59 -0400

    fs ways done

---
+++ fs.js
@@ -0,0 +1,54 @@
+// import * as fs from "node:fs";
+import * as fs from "node:fs/promises";
+
+//^ Synchronous way of writing code ^
+export function createFile(pathname) {
+  fs.writeFileSync(pathname, "Hello Node js");
+  fs.appendFileSync(pathname, " from Rishabh");
+  console.log("File has been created successfully!");
+}
+// createFile('./hello.txt')
+
+//------------------------------------------------------------------
+
+//^ Asynchronous way of writing code ^
+
+export function createFileAsync(pathname) {
+  fs.writeFile(pathname, "Hello node Js", (error) => {
+    // Err first call back in Node Js
+    if (error) {
+      console.log("Something went wrong!");
+    } else {
+      console.log("File has been created successfully!");
+      fs.appendFile(pathname, "\nHello Js", (err) => {
+        if (err) {
+          console.log("Something went wrong!");
+        } else {
+          console.log("File appended successfully!");
+        }
+      });
+    }
+  });
+  console.log("File operation done!");
+}
+
+// createFileAsync("./hello.txt");
+
+//----------------------------------------------------------------------------------
+
+// ^ Promise way of writing a code ^
+
+export async function createFilePromise(pathname) {
+  try {
+    await fs.writeFile(pathname, "Hello Node js");
+    console.log("File has been created successfully!");
+    await fs.appendFile(pathname, "\nAppended successfilly");
+    console.log("Appended successfully!");
+  } catch (err) {
+    console.error("Something went wrong", err);
+  } finally {
+    console.log("File operation Done");
+  }
+}
+
+createFilePromise("./hello.txt");
